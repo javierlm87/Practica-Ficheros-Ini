68179d3dac756d3db1e6b1ac91bedf15f846f495
javi <javier_lm87@hotmail.com>
2013-03-06 19:27:47 +0000

    commit final Practica Ficheros Ini

--- ini.js
+++ ini.js
@@ -21,53 +21,53 @@ function calculate(evt) {
     }
     r.readAsText(f); // Leer como texto 
   } else { 
-    alert("Failed to load file");
+    alert("Fallo al cargar el fichero");
   }
 }
 
-var temp = '<li> <span class = "<%= _____ %>"> <%= _ %> </span>\n';
+var temp = '<li> <span class = "<%= token.type %>"> <%= match %> </span>\n';
 
 function tokensToString(tokens) {
    var r = '';
    for(var i in tokens) {
      var t = tokens[i];
      var s = JSON.stringify(t, undefined, 2);
-     s = _.template(temp, {t: t, s: s});
+     s = _.template(temp, {token: t, match: s});
      r += s;
    }
    return '<ol>\n'+r+'</ol>';
 }
 
 function lexer(input) {
-  var blanks         = /^\s+$/;
-  var iniheader      = /^\[[a-zA-Z_]\w*\]$/;
+  var blanks         = /^\s+/;
+  var iniheader      = /^\[[a-zA-Z_]\w*\]/;
   var comments       = /^;.*/;
   var nameEqualValue = /^[a-zA-Z_]\w*\s*(?:=)/;
-  var any            = /^(.|\n)+/;
+  var any            = /^(.)+/;
 
   var out = [];
   var m = null;
 
   while (input != '') {
     if (m = blanks.exec(input)) {
-      input = input.substr(m.index+___________);
-      out.push({ type : ________, match: _ });
+      input = input.substr(m.index + m[0].length);
+      out.push({ type: "blanks", match: m[0] });    
     }
     else if (m = iniheader.exec(input)) {
-      input = input.substr(___________________);
-      _______________________________________ // avanzemos en input
+      input = input.substr(m.index + m[0].length);
+      out.push({ type: "iniheader", match: m[0] }); // avanzemos en input
     }
     else if (m = comments.exec(input)) {
-      input = input.substr(___________________);
-      _________________________________________
+     input = input.substr(m.index + m[0].length);
+     out.push({ type: "comments", match: m[0] });
     }
     else if (m = nameEqualValue.exec(input)) {
-      input = input.substr(___________________);
-      _______________________________________________
+      input = input.substr(m.index + m[0].length);
+      out.push({ type: "nameEqualValue", match: m[0] });
     }
     else if (m = any.exec(input)) {
-      _______________________________________
-      input = '';
+      input = input.substr(m.index + m[0].length);
+      out.push ({type: "any", match: m[0]});
     }
     else {
       alert("Fatal Error!"+substr(input,0,20));
